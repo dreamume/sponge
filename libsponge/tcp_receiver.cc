0e350c25e2808c6200e4e12e3634709c5ddd9687
dreamume <dreamume@gmail.com>
2023-03-08 14:43:38 +0800

    feat(lab2): complete lab2, but test not all passed

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,26 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+    if (!_syn_flag) {
+        if (!header.syn) return;
+        _syn_flag = header.syn;
+        _isn = header.seqno;
+    }
+    uint64_t checkpoint = header.syn ? 0 : stream_out().bytes_written() - 1;
+    uint64_t abs_seqno = unwrap(header.seqno, _isn, checkpoint);
+    uint64_t stream_idx = header.syn ? 0 : abs_seqno - 1;
+    _reassembler.push_substring(seg.payload().copy(), stream_idx, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if (!_syn_flag) return std::nullopt;
+    uint64_t abs_ackno = stream_out().bytes_written() + 1;
+    if (stream_out().input_ended()) abs_ackno += 1;
 
-size_t TCPReceiver::window_size() const { return {}; }
+    return wrap(abs_ackno, _isn);
+}
+
+size_t TCPReceiver::window_size() const { 
+    return _capacity - stream_out().buffer_size(); 
+}
